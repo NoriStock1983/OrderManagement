2b753a94193013f4ab7fc020b10c2f7269cd2503
AQUA651 <86642639+NoriStock1983@users.noreply.github.com>
2025-08-20 22:02:18 +0900

    add repository

---
+++ src/domain/repositories/compnanymst/companymstRepository.py
@@ -0,0 +1,38 @@
+from sqlalchemy import text
+
+
+class getCompanymst:
+    def getAllCompanycode_and_name(self) -> None:
+        query = text("")
+        try:
+            print(query)
+        except Exception as e:
+            print(e)
+        finally:
+            print("Test Finally")
+
+    def getAllCompanymst(self) -> None:
+        query = text("")
+        print(query)
+
+    def getByComapnycd(self) -> None:
+        query = text("")
+        print(query)
+
+
+class updateCompanymst:
+    def updateByCompnaycd(self) -> None:
+        query = text("")
+        print(query)
+
+
+class insertCompanyst:
+    def insertByCompnaycd(self) -> None:
+        query = text("")
+        print(query)
+
+
+class deleteCompanyst:
+    def deleteByCompnaycd(self) -> None:
+        query = text("")
+        print(query)
